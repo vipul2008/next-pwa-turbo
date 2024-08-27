3ebf11ba05298aa44cb348d33ae02554cdb7d827
Vipul Goel <vipul2008@gmail.com>
2024-08-27 18:17:47 +0530

    PWA Setup completed

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
+
+const nextConfig = {
+	reactStrictMode: true,
+	transpilePackages: ["@repo/ui"],
+};
+
+// You may want to use a more robust revision to cache
+// files more efficiently.
+// A viable option is `git rev-parse HEAD`.
+const revision = crypto.randomUUID();
+
+module.exports = async () => {
+	const withSerwist = (await import("@serwist/next")).default({
+		cacheOnNavigation: true,
+		swSrc: "src/app/sw.ts",
+		swDest: "public/sw.js",
+		additionalPrecacheEntries: [{ url: "/~offline", revision }],
+	});
+	return withSerwist(nextConfig);
 };
